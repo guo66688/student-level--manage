76ab9188495f5fb087d2b39724335abae143cdf3
icoffee <1596740959@qq.com>
2025-05-28 16:00:32 +0800

    fix_student_mysql_uniqueIdex_no_use_text_need_length

--- models/student.go
+++ models/student.go
@@ -4,9 +4,9 @@ import "gorm.io/gorm"
 
 type Student struct {
 	gorm.Model
-	StudentNo string `gorm:"uniqueIndex"` // 学号唯一
-	Name      string
-	Gender    string
-	BirthDate string
-	ClassName string
+	StudentNo string `gorm:"type:varchar(255);uniqueIndex"`
+	Name      string `gorm:"type:varchar(255)"`
+	Gender    string `gorm:"type:varchar(50)"`
+	BirthDate string `gorm:"type:varchar(255)"`
+	ClassName string `gorm:"type:varchar(255)"`
 }
